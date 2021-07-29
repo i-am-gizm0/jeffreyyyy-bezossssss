c4ab7844b2c8c30965902f5ecaddceb2824752e3
Sam <centersam19@gmail.com>
2021-07-28 23:53:23 -0400

    Force using docker
    HELP I DON"T KNOW HOW BASH WORKS

--- youdidit.sh
+++ youdidit.sh
@@ -2,19 +2,20 @@
 #     sudo ./install.sh
 # fi
 
-if docker ps | grep pihole; then
-    # Pihole is running in a container
-    GET_LOGS_CMD="docker exec pihole -t | grep --line-buffered device-metrics-us.amazon.com"
-elif locate pihole; then
-    # Pihole is installed
-    GET_LOGS_CMD="pihole -t | grep --line-buffered device-metrics-us.amazon.com"
-else
-    # Pihole is not installed
-    echo "Pihole is not installed"
-    exit 1
-fi
+#if docker ps | grep pihole; then
+#    # Pihole is running in a container
+#    GET_LOGS_CMD="docker exec pihole -t | grep --line-buffered device-metrics-us.amazon.com"
+#elif locate pihole; then
+#    # Pihole is installed
+#    GET_LOGS_CMD="pihole -t | grep --line-buffered device-metrics-us.amazon.com"
+#else
+#    # Pihole is not installed
+#    echo "Pihole is not installed"
+#    exit 1
+#fi
 
-exec "$GET_LOGS_CMD" | (
+# exec "$GET_LOGS_CMD"
+docker exec pihole pihole -t | grep --line-buffered device-metrics-us.amazon.com | (
     while read -r line; do
         # Play
         mpg123 bezos.mp3 &
